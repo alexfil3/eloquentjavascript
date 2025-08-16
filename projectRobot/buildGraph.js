f0b5aa3ebafea552f4bc7e7b2d61a457bce84a37
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 16:37:00 +0300

    add graph argument to addEdge call

--- projectRobot/buildGraph.js
+++ projectRobot/buildGraph.js
@@ -1,14 +1,17 @@
+import roads from "./roads.js";
 import addEdge from "./addEdge.js";
 
 function buildGraph(edges) {
     let graph = Object.create(null);
 
     for (let [from, to] of edges.map(r => r.split("-"))) {
-        addEdge(from, to);
-        addEdge(to, from);
+        addEdge(from, to, graph);
+        addEdge(to, from, graph);
     }
     
     return graph;
 }
 
-export default buildGraph;
+const roadGraph = new buildGraph(roads);
+
+export default roadGraph;
