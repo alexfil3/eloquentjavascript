bccd1469ec77a15f72c4bfabe68c6b3a86bc4da3
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 14:45:38 +0300

    removes previously moved to a separete file function addEdge

--- projectRobot/buildGraph.js
+++ projectRobot/buildGraph.js
@@ -1,13 +1,7 @@
+import addEdge from "./addEdge.js";
+
 function buildGraph(edges) {
     let graph = Object.create(null);
-
-    function addEdge(from, to) {
-        if (from in graph) {
-            graph[from].push(to);
-        } else {
-            graph[from] = [to];
-        }
-    }
 
     for (let [from, to] of edges.map(r => r.split("-"))) {
         addEdge(from, to);
