bb634547e4a39fe87a3903c960ef89caa2625483
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 17:12:39 +0300

    add index.js and runRobot

--- projectRobot/index.js
+++ projectRobot/index.js
@@ -1,14 +1,5 @@
 import VillageState from "./VillageState.js";
+import runRobot from "./runRobot.js";
+import randomRobot from "./randomRobot.js";
 
-let first = new VillageState(
-    "Post Office",
-    [{place: "Post Office", address: "Alice's House"}]
-);
-let next = first.move("Alice's House");
-
-console.log(next.place);
-// → Alice's House
-console.log(next.parcels);
-// → []
-console.log(first.place);
-// → Post Office
+runRobot(VillageState.random(), randomRobot);
