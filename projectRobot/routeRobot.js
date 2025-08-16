a9dbe54696947fe0e0a027cc0b8874acef02f446
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 18:50:06 +0300

    add routeRobot and add extra line to mailRoute

---
+++ projectRobot/routeRobot.js
@@ -0,0 +1,9 @@
+function routeRobot(state, memory) {
+    if (memory.length == 0) {
+        memory = mailRoute;
+    }
+
+    return {direction: memory[0], memory: memory.slice(1)};
+}
+
+export default routeRobot;
