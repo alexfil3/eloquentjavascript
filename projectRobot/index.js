ebf599bd5d953a52ac66b0d23041b3bb42e79950
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 16:37:19 +0300

    create index.js

---
+++ projectRobot/index.js
@@ -0,0 +1,14 @@
+import VillageState from "./VillageState.js";
+
+let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's House"}]
+);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office
