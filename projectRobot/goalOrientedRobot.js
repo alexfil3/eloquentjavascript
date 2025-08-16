482bb560265c1942930fc4ea1754c1f0f46ae59b
Alex Filimonov <filimonovalexander9997@gmail.com>
2025-08-16 19:03:54 +0300

    add goalOrientedRobot

---
+++ projectRobot/goalOrientedRobot.js
@@ -0,0 +1,18 @@
+import findRoute from "./findRoute.js";
+import roadGraph from "./buildGraph.js";
+
+function goalOrientedRobot({ place, parcels }, route) {
+    if (route.length == 0) {
+        let parcel = parcels[0];
+
+        if (parcel.place != place) {
+            route = findRoute(roadGraph, place, parcel.place);
+        } else {
+            route = findRoute(roadGraph, place, parcel.address);
+        }
+    }
+
+    return {direction: route[0], memory: route.slice(1)};
+}
+
+export default goalOrientedRobot;
--- projectRobot/index.js
+++ projectRobot/index.js
@@ -2,5 +2,6 @@ import VillageState from "./VillageState.js";
 import runRobot from "./runRobot.js";
 import randomRobot from "./randomRobot.js";
 import routeRobot from "./routeRobot.js";
+import goalOrientedRobot from "./goalOrientedRobot.js";
 
-runRobot(VillageState.random(), routeRobot, []);
+runRobot(VillageState.random(), goalOrientedRobot, []);
